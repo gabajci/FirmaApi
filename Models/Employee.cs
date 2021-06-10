1b6afd1b33b0fd5d0b4072a845e772a5656edc91
adam <ratkovskya333@gmail.com>
2021-06-10 19:51:16 +0200

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FirmaApi.Models
+{
+    public partial class Employee
+    {
+
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string LastName { get; set; }
+        public DateTime SignInDate { get; set; }
+        public DateTime? LeaveDate { get; set; }
+        public string Title { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+    }
+}
